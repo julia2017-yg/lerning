9d075e72ff6392a65fac87ac349da202ca91f32b
julia <julka.getman@gmail.com>
2020-11-02 15:56:15 +0200

    second practice

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
 console.log(numberOfFilms);
 
@@ -9,13 +11,28 @@ const personalMovieDB = {
            privat: false
 };
 
-const q1 = prompt('Один из последних просмотренных фильмов', ''),
-    q2 = prompt('На сколько оцените его', ''),
-    q3 = prompt('Один из последних просмотренных фильмов', ''),
-    q4 = prompt('На сколько оцените его', '');
+    for(let i = 0; i < 2; i++) {
+      const q1 = prompt('Один из последних просмотренных фильмов', ''),
+            q2 = prompt('На сколько оцените его', '');
 
-    personalMovieDB.movies[q1] = q2;
-    personalMovieDB.movies[q3] = q4;
+            if (q1 != null && q2 != null && q1 != '' && q2 != '' && q1.length < 50) {
+                 personalMovieDB.movies[q1] = q2;
+                 console.log('done');
+            }else {
+              console.log('error');
+              i--;
+            }        
+    }
+
+if (personalMovieDB.count < 10){
+  console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+  console.log('ВЫ классический зритель');
+} else if (personalMovieDB.count >= 30){
+  console.log('ВЫ киноман');
+}else {
+  console.log('Error');
+}
 
   console.log(personalMovieDB);
    
